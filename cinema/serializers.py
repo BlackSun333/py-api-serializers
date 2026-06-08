154f38b362d586bc3af94caeec3e130b2d9d781c
Stanislav <staskobetc@gmail.com>
2026-06-08 09:45:44 +0300

    Solution

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -57,9 +57,12 @@ class MovieCreateSerializer(serializers.ModelSerializer):
 
 
 class MovieSessionListSerializer(serializers.ModelSerializer):
-    movie_title = serializers.CharField(source="movie.title", read_only=True)
-    cinema_hall_name = serializers.CharField(source="cinema_hall.name", read_only=True)
-    cinema_hall_capacity = serializers.IntegerField(source="cinema_hall.capacity", read_only=True)
+    movie_title = (serializers.CharField
+                   (source="movie.title", read_only=True))
+    cinema_hall_name = (serializers.CharField
+                        (source="cinema_hall.name", read_only=True))
+    cinema_hall_capacity = (serializers.IntegerField
+                            (source="cinema_hall.capacity", read_only=True))
 
     class Meta:
         model = MovieSession
